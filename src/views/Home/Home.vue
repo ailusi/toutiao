82306ba20cfe35ad439d12cf5cfc4a7aff1292a5
mik <1284505150@qq.com>
2022-06-16 14:31:41 +0800

    实现了频道的增加和删除管理

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -15,13 +15,72 @@
         <ArtList :channel-id="item.id" class="list"></ArtList>
       </van-tab>
       <!-- 频道管理的小图标 -->
-      <van-icon name="plus" size="16" class="plus" />
+      <van-icon name="plus" size="16" class="plus" @click="showPopup" @closed="isDel = false" />
+      <!-- 频道弹出层 -->
+      <van-popup v-model="show" :close-on-click-overlay="false">
+        <div class="popup-container">
+          <!-- 弹出层的头部区域 -->
+          <van-nav-bar title="频道管理">
+            <template #right>
+              <van-icon name="cross" size="14" color="white" @click="show = false" />
+            </template>
+          </van-nav-bar>
+
+          <!-- 弹出层的主体区域 -->
+          <div class="pop-body">
+            <!-- 我的频道 -->
+            <div class="my-channel-box">
+              <div class="channel-title">
+                <div>
+                  <span class="title-bold">已添加频道：</span>
+                  <span class="title-gray">{{ isDel ? '点击移除频道' : '点击进入频道' }}</span>
+                </div>
+                <span class="btn-edit" @click="isDel = !isDel">{{ isDel ? '完成' : '编辑' }}</span>
+              </div>
+              <!-- 我的频道列表 -->
+              <van-row type="flex">
+                <van-col span="6" v-for="(item, index) in userChannel" :key="item.id">
+                  <!-- 用户的频道 Item 项 -->
+                  <div class="channel-item van-hairline--surround" @click="onUserChannelClick(item, index)">
+                    {{ item.name }}
+                    <!-- 删除的图标 -->
+                    <van-badge color="transparent" class="cross-badge" v-if="isDel && item.name !== '推荐' && userChannel.length > 2">
+                      <template #content>
+                        <van-icon name="cross" class="badge-icon" color="#cfcfcf" size="12" />
+                      </template>
+                    </van-badge>
+                  </div>
+                </van-col>
+              </van-row>
+            </div>
+
+            <!-- 分隔线 -->
+            <div class="van-hairline--top sp-line"></div>
+
+            <!-- 更多频道 -->
+            <div class="more-channel-box">
+              <div class="channel-title">
+                <div>
+                  <span class="title-bold">可添加频道：</span>
+                  <span class="title-gray">点击添加频道</span>
+                </div>
+              </div>
+              <!-- 更多频道列表 -->
+              <van-row type="flex">
+                <van-col span="6" v-for="item in moreChannels" :key="item.id">
+                  <div class="channel-item van-hairline--surround" @click="addChannel(item)">{{ item.name }}</div>
+                </van-col>
+              </van-row>
+            </div>
+          </div>
+        </div>
+      </van-popup>
     </van-tabs>
   </div>
 </template>
 
 <script>
-import { getUserChannelAPI } from '@/api/homeApi'
+import { getUserChannelAPI, getAllChannelAPI, updateUserChannelAPI } from '@/api/homeApi'
 import ArtList from '@/components/ArtList/ArtList.vue'
 export default {
   components: { ArtList },
@@ -29,11 +88,20 @@ export default {
   data() {
     return {
       active: 0,
-      userChannel: []
+      userChannel: [],
+      // 管理弹出层的弹出和隐藏
+      show: false,
+      // 所有的频道列表数据
+      allChannels: [],
+      // 频道数据是否处于删除的状态
+      isDel: false
     }
   },
   created() {
+    // 请求用户的频道列表数据
     this.initUserChannel()
+    // 请求所有的频道列表数据
+    this.initAllChannel()
   },
   methods: {
     async initUserChannel() {
@@ -41,6 +109,93 @@ export default {
       if (res.message === 'OK') {
         this.userChannel = res.data.channels
       }
+    },
+    showPopup() {
+      this.show = true
+    },
+    // 获取所有频道的列表数据
+    async initAllChannel() {
+      const { data: res } = await getAllChannelAPI()
+      if (res.message === 'OK') {
+        // 将请求到的数据，转存到 allChannel 中
+        this.allChannels = res.data.channels
+      }
+    },
+    // 新增频道（形参 item 就是当前用户点击的频道信息对象）
+    // 格式：{ id, name }
+    addChannel(item) {
+      // 将用户点击的频道，添加到“用户频道”列表中
+      this.userChannel.push(item)
+
+      // TODO：调用 API 接口，将最新的用户频道，存储到后台数据库中
+      this.updateChannel()
+    },
+    // 把用户的频道列表数据提交到后端保存
+    async updateChannel(item) {
+      // 1. 准备要提交到服务器的数据
+      const channels = this.userChannel
+        .filter((item) => item.name !== '推荐') // 将“推荐”从频道列表中过滤出去
+        .map((item, index) => {
+          // 调用数组的 map 循环，最终返回一个处理好的新数组
+          return {
+            id: item.id,
+            seq: index + 1
+          }
+        })
+
+      // 2. 调用 API 接口
+      const { data: res } = await updateUserChannelAPI(channels)
+      if (res.message === 'OK') {
+        // TODO：提示用户更新成功
+        // 3. 通过 notify 弹框提示用户更新成功
+        this.$notify({ type: 'success', message: '更新成功', duration: 1000 })
+      }
+    },
+    // 从用户频道列表中，移除指定 id 的频道
+    onUserChannelClick(channel, index) {
+      if (this.isDel) {
+        // 处于删除状态
+
+        if (channel.name === '推荐' || this.userChannel.length === 2) return
+        // 进行数组的过滤
+        this.userChannel = this.userChannel.filter((item) => item.id !== channel.id)
+        // 将更改过后的用户频道数据，提交到服务器保存
+        this.updateChannel()
+      } else {
+        // 不处于删除状态
+
+        // 1. 修改 Tabs 的激活项的索引值
+        this.active = index
+        // 2. 关闭 popup 弹出层
+        this.show = false
+      }
+    },
+    // 从用户频道列表中，移除指定 id 的频道
+    removeChannel(channel) {
+      if (this.isDel) {
+        // 处于删除状态
+        // 判断用户点击的是否为“推荐”
+        if (channel.name === '推荐' || this.userChannel.length === 2) return
+
+        // 移除频道
+        this.userChannel = this.userChannel.filter((item) => item.id !== channel.id)
+        this.updateChannel()
+      } else {
+        // 不处于删除状态
+      }
+    }
+  },
+  computed: {
+    // 更多频道的数据
+    moreChannels() {
+      // 1. 对数组进行 filter 过滤，返回的是符合条件的新数组
+      // eslint-disable-next-line array-callback-return
+      return this.allChannels.filter((item) => {
+        // 判断当前循环项，是否在 “我的频道” 列表之中
+        const index = this.userChannel.findIndex((x) => x.id === item.id)
+        // 如果不在，则 return true，表示需要把这一项存储到返回的新数组之中
+        if (index === -1) return true
+      })
     }
   }
 }
@@ -68,5 +223,69 @@ export default {
 }
 .list {
   padding-top: 40px;
+}
+.van-popup,
+.popup-container {
+  background-color: transparent;
+  height: 100%;
+  width: 100%;
+}
+
+.popup-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.pop-body {
+  flex: 1;
+  overflow: scroll;
+  padding: 8px;
+  background-color: white;
+}
+
+.my-channel-box,
+.more-channel-box {
+  .channel-title {
+    display: flex;
+    justify-content: space-between;
+    font-size: 14px;
+    line-height: 28px;
+    padding: 0 6px;
+    .title-bold {
+      font-weight: bold;
+    }
+    .title-gray {
+      color: gray;
+      font-size: 12px;
+    }
+  }
+}
+
+.btn-edit {
+  border: 1px solid #a3a2a2;
+  padding: 0 10px;
+  line-height: 20px;
+  height: 20px;
+  border-radius: 6px;
+  font-size: 12px;
+}
+
+.channel-item {
+  font-size: 12px;
+  text-align: center;
+  line-height: 36px;
+  background-color: #fafafa;
+  margin: 6px;
+}
+
+.cross-badge {
+  position: absolute;
+  right: -3px;
+  top: 0;
+  border: none;
+}
+
+.sp-line {
+  margin: 10px 0 20px 0;
 }
 </style>
